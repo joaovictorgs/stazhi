dc217a713b99dcbf34f7f0637bb6ab55ffa0edc7
joaovictorgs <joaovictorgodoydasilva@gmail.com>
2025-06-14 17:37:34 -0300

    feat: adding validator

--- br/inatel/stazhi/util/validador/Validador.java
+++ br/inatel/stazhi/util/validador/Validador.java
@@ -1,5 +1,58 @@
 package br.inatel.stazhi.util.validador;
 
+import java.util.regex.Pattern;
+
+import br.inatel.stazhi.execoes.CNPJInvalidoException.CNPJInvalidoException;
+
 public class Validador {
-    
+
+    public static void CNPJ(String cnpj) throws CNPJInvalidoException{
+          if (cnpj == null) throw new CNPJInvalidoException("CNPJ Error, CNPJ não inserido. O formato correto deve ser XX.XXX.XXX/0001-ZZ");
+
+        // Remove caracteres não numéricos
+        cnpj = cnpj.replaceAll("[^\\d]", "");
+
+        // Verifica se tem 14 dígitos
+        if (cnpj.length() != 14) throw new CNPJInvalidoException("CNPJ Error, CNPJ de tamanho inválido. O formato correto deve ser XX.XXX.XXX/0001-ZZ");
+
+        // Verifica se todos os dígitos são iguais (ex: 00000000000000)
+        if (cnpj.matches("(\\d)\\1{13}"))  throw new CNPJInvalidoException("CNPJ Error, o valores não está de acordo. O formato correto deve ser XX.XXX.XXX/0001-ZZ ");
+
+        //verifica se é valido o cnpj no caso de um cnpj no padrão passar pelas demais verificações
+        try {
+            int[] pesos1 = {5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+            int[] pesos2 = {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+
+            String cnpjBase = cnpj.substring(0, 12);
+            String digitos = cnpj.substring(12, 14);
+
+            int primeiroDigito = calcularDigito(cnpjBase, pesos1);
+            int segundoDigito = calcularDigito(cnpjBase + primeiroDigito, pesos2);
+
+            if(!digitos.equals("" + primeiroDigito + segundoDigito)){
+                throw new CNPJInvalidoException("CNPJ Error, CNPJ inválido. O formato correto deve ser XX.XXX.XXX/0001-ZZ");
+            }
+        } catch (Exception e) {
+            throw new CNPJInvalidoException("CNPJ Error, CNPJ inválido. O formato correto deve ser XX.XXX.XXX/0001-ZZ");
+        }
+    }
+
+    private static final Pattern EMAIL_REGEX = Pattern.compile(
+        "^[\\w\\.-]+@[\\w\\.-]+\\.[a-zA-Z]{2,}$"
+    );
+
+    public static boolean Email(String email) {
+        if (email == null) return false;
+        return EMAIL_REGEX.matcher(email).matches();
+    }
+
+
+    private static int calcularDigito(String str, int[] pesos) {
+        int soma = 0;
+        for (int i = 0; i < pesos.length; i++) {
+            soma += Character.getNumericValue(str.charAt(i)) * pesos[i];
+        }
+        int resto = soma % 11;
+        return (resto < 2) ? 0 : 11 - resto;
+    }
 }
