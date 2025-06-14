c7e370d129e56e8f1f1b75574d39ee9c964908de
joaovictorgs <joaovictorgodoydasilva@gmail.com>
2025-06-14 16:39:50 -0300

    feat: creating vaga class

---
+++ br/inatel/stazhi/Enum/modalidade/Modalidade.java
@@ -0,0 +1,8 @@
+package br.inatel.stazhi.Enum.modalidade;
+
+public enum Modalidade {
+    presencial,
+    hibrido,
+    remoto,
+
+}
--- br/inatel/stazhi/model/vaga/Vaga.java
+++ br/inatel/stazhi/model/vaga/Vaga.java
@@ -1,5 +1,42 @@
 package br.inatel.stazhi.model.vaga;
 
+import java.text.DateFormat;
+
+import br.inatel.stazhi.Enum.modalidade.Modalidade;
+
+
 public class Vaga {
-    
+    private int id;
+    private String descricao;
+    private int quantidadeDeCandidaturas;
+    private DateFormat dataLimite;
+    private Modalidade modalidade;
+
+    public Vaga(int id, String descricao, int quantidadeDeCandidaturas, DateFormat dataLimite, Modalidade modalidade){
+        this.id = id;
+        this.descricao = descricao;
+        this.quantidadeDeCandidaturas = quantidadeDeCandidaturas;
+        this.dataLimite = dataLimite;
+        this.modalidade = modalidade;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public String getDescricao(){
+        return this.descricao;
+    }
+
+    public int getQuantidadeDeCandidaturas(){
+        return this.quantidadeDeCandidaturas;
+    }
+
+    public DateFormat getDataLimite(){
+        return this.dataLimite;
+    }
+
+    public Modalidade getModalidade(){
+        return this.modalidade;
+    }
 }
