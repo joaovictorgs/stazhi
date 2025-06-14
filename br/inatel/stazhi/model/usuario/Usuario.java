e0112594e2203cfecced3ce16c74fdf0695b7cac
joaovictorgs <joaovictorgodoydasilva@gmail.com>
2025-06-14 15:50:45 -0300

    feat: adding senha atribute to usuario

--- br/inatel/stazhi/model/usuario/Usuario.java
+++ br/inatel/stazhi/model/usuario/Usuario.java
@@ -1,5 +1,19 @@
 package br.inatel.stazhi.model.usuario;
 
-public abstract class Usuario {
+import br.inatel.stazhi.interfaces.autenticavel.Autenticavel;
 
+public abstract class Usuario implements Autenticavel{
+    protected String nome;
+    protected String senha;
+    protected String email;
+    protected int id;
+
+    public Usuario(int id, String nome, String senha, String email){
+        this.nome = nome;
+        this.senha = senha;
+        this.id = id;
+        this.email = email;
+    }
+    
+    public void showMenu(){}
 }
