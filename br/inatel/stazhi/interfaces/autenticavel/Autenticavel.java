cfbe6d9c0582e0a7f0fa72c817e39eeee317aa8f
joaovictorgs <joaovictorgodoydasilva@gmail.com>
2025-06-14 14:55:03 -0300

    feat: adding interfaces folder

---
+++ br/inatel/stazhi/interfaces/autenticavel/Autenticavel.java
@@ -0,0 +1,5 @@
+package br.inatel.stazhi.interfaces.autenticavel;
+
+public class Autenticavel {
+    
+}
---
+++ br/inatel/stazhi/interfaces/gerenciadorDeDados/GerenciadorDeDados.java
@@ -0,0 +1,5 @@
+package br.inatel.stazhi.interfaces.gerenciadorDeDados;
+
+public class GerenciadorDeDados {
+    
+}
