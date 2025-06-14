1903ed6c5e5bbb56969a8c860a6ba57ff331eed0
joaovictorgs <joaovictorgodoydasilva@gmail.com>
2025-06-14 16:47:55 -0300

    feat: creating candidatura object

--- br/inatel/stazhi/model/candidatura/Candidatura.java
+++ br/inatel/stazhi/model/candidatura/Candidatura.java
@@ -1,5 +1,22 @@
 package br.inatel.stazhi.model.candidatura;
 
+import br.inatel.stazhi.model.aluno.Aluno;
+import br.inatel.stazhi.model.vaga.Vaga;
+
 public class Candidatura {
-    
+    private Vaga vaga;
+    private Aluno aluno;
+
+    public Candidatura(Vaga vaga, Aluno aluno){
+        this.vaga = vaga;
+        this.aluno = aluno;
+    }
+
+    public Vaga getVaga(){
+        return this.vaga;
+    }
+
+    public Aluno getAluno(){
+        return this.aluno;
+    }
 }
