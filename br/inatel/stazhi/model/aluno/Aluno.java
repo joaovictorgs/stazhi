c0b0c25913c5dcd9632b760d45b7be14db53dda5
joaovictorgs <joaovictorgodoydasilva@gmail.com>
2025-06-14 16:03:09 -0300

    feat: adding aluno class constructor and some gets

--- br/inatel/stazhi/model/aluno/Aluno.java
+++ br/inatel/stazhi/model/aluno/Aluno.java
@@ -3,10 +3,13 @@ package br.inatel.stazhi.model.aluno;
 import br.inatel.stazhi.model.usuario.Usuario;
 
 public class Aluno extends Usuario {
+    private int idade;
+    private String formacao;
 
-    public Aluno(int id, String nome, String email) {
-        super(id, nome, email);
-        //TODO Auto-generated constructor stub
+    public Aluno(int id, String nome, String senha, int idade, String formacao, String email) {
+        super(id, nome, senha, email);
+        this.idade = idade;
+        this.formacao = formacao;
     }
 
     @Override
@@ -19,6 +22,24 @@ public class Aluno extends Usuario {
     public void cadastro(String email, String senha) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'cadastro'");
+    }
+
+    @Override
+    public void showMenu() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'showMenu'");
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+    
+    public String getSenha(){
+        return this.senha;
     }
 
 }
