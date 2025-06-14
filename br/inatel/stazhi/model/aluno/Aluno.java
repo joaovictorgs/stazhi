584d4a2cef2bf37a94b2d6c15996512bec45bac1
joaovictorgs <joaovictorgodoydasilva@gmail.com>
2025-06-14 15:46:52 -0300

    feat: adding user as an abstract class

--- br/inatel/stazhi/model/aluno/Aluno.java
+++ br/inatel/stazhi/model/aluno/Aluno.java
@@ -4,4 +4,21 @@ import br.inatel.stazhi.model.usuario.Usuario;
 
 public class Aluno extends Usuario {
 
+    public Aluno(int id, String nome, String email) {
+        super(id, nome, email);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void login(String email, String senha) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'login'");
+    }
+
+    @Override
+    public void cadastro(String email, String senha) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cadastro'");
+    }
+
 }
