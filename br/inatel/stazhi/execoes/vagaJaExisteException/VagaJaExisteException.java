acbfee629c0d8966a6e58c32e9a7b33d178b52cd
joaovictorgs <joaovictorgodoydasilva@gmail.com>
2025-06-15 14:15:17 -0300

    feat: creating exception

---
+++ br/inatel/stazhi/execoes/vagaJaExisteException/VagaJaExisteException.java
@@ -0,0 +1,7 @@
+package br.inatel.stazhi.execoes.vagaJaExisteException;
+
+public class VagaJaExisteException extends Exception {
+    public VagaJaExisteException(String mensagem) {
+        super(mensagem);
+    }
+}
