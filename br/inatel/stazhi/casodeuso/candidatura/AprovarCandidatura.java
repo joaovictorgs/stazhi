5ebfb86a924db68aa8f255faa256cd88cbe49d08
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2025-06-15 21:11:50 -0300

    feat: deletar vaga quando aprovar o aluno

--- br/inatel/stazhi/casodeuso/candidatura/AprovarCandidatura.java
+++ br/inatel/stazhi/casodeuso/candidatura/AprovarCandidatura.java
@@ -1,17 +1,16 @@
 package br.inatel.stazhi.casodeuso.candidatura;
 
 import br.inatel.stazhi.repositorio.candidaturaRepositorio.CandidaturaRepositorio;
+import br.inatel.stazhi.repositorio.vagaRepositorio.VagaRepositorio;
 
 public class AprovarCandidatura {
     
     public void executar(int idAluno, int idVaga) throws Exception {
         CandidaturaRepositorio candidaturaRepositorio = new CandidaturaRepositorio();
-        
-        if (!candidaturaRepositorio.existePorId(idVaga, idAluno)) {
-            throw new Exception("Candidatura não encontrada para o aluno e vaga especificados.");
-        }
+        VagaRepositorio vagaRepositorio = new VagaRepositorio();
         
         candidaturaRepositorio.deletarPorVaga(idVaga);
         
+        vagaRepositorio.deletar(idVaga);
     }
 }
