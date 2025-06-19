a05f0f02fb7782469182c34835e3dc0d1f4cf44a
joaovictorgs <joaovictorgodoydasilva@gmail.com>
2025-06-19 12:03:06 -0300

    feat: adding the method to list  the alunos of an supervisor on the supervisionamento cli

--- br/inatel/stazhi/cli/SupervisionamentoCLI.java
+++ br/inatel/stazhi/cli/SupervisionamentoCLI.java
@@ -3,11 +3,10 @@ package br.inatel.stazhi.cli;
 import java.util.List;
 import java.util.Scanner;
 
-import br.inatel.stazhi.casodeuso.candidatura.AprovarCandidatura;
 import br.inatel.stazhi.casodeuso.supervisor.SupervisionarAluno;
 import br.inatel.stazhi.model.aluno.Aluno;
 import br.inatel.stazhi.repositorio.alunoRepositorio.AlunoRepositorio;
-import br.inatel.stazhi.repositorio.vagaRepositorio.VagaRepositorio;
+
 
 public class SupervisionamentoCLI {
 
@@ -53,6 +52,44 @@ public class SupervisionamentoCLI {
                 }
             }else{
                 System.out.println("Insira um valor valido");
+            }
+        }
+    }
+
+    public static void ListarAlunosSupervisionados(int supervisorId){
+        List<Aluno> alunos = new AlunoRepositorio().listarSupervisionadosPorUmId(supervisorId);
+        if(alunos.isEmpty()){
+            System.out.println("Não existem alunos sem supervisores no momento");
+            return;
+        }
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("=== Alunos Supervisionados ===");
+            for(int i =0; i < alunos.size(); i++){
+                Aluno aluno = alunos.get(i);
+                System.out.println("\nAluno " + (i + 1) + ":");
+                System.out.println("Nome: " + aluno.getNome());
+                System.out.println("Email: "+ aluno.getEmail());
+            }
+            System.out.println("\n Digite o número do aluno para ver mais detalhes sobre (0 para sair):");
+            int alunoSelecionado = scanner.nextInt();
+            scanner.nextLine();
+            if(alunoSelecionado == 0){
+                break;
+            }
+            if (alunoSelecionado > 0 && alunoSelecionado <= alunos.size()) {
+                Aluno aluno = alunos.get(alunoSelecionado -1);
+                System.out.println("Nome: " + aluno.getNome());
+                System.out.println("Idade: " + aluno.getIdade());
+                System.out.println("Formação: " + aluno.getFormacao());
+                System.out.println("Email: "+ aluno.getEmail());
+            }else{
+                System.out.println("Valor inválido");
+            }
+            System.out.println("\nDeseja verificar outro aluno? (s/n):");
+            String resposta = scanner.nextLine().trim().toLowerCase();
+            if (resposta.equals("n")) {
+                break;
             }
         }
     }
