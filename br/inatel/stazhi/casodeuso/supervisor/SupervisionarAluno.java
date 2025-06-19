d3753dd21d84a41dc029ec10b9a3e0c20e06fc82
joaovictorgs <joaovictorgodoydasilva@gmail.com>
2025-06-19 01:09:01 -0300

    fix: removing the throw from the supervisionarAluno  because all the verifications where running before the of the casodeuso

--- br/inatel/stazhi/casodeuso/supervisor/SupervisionarAluno.java
+++ br/inatel/stazhi/casodeuso/supervisor/SupervisionarAluno.java
@@ -1,12 +1,9 @@
 package br.inatel.stazhi.casodeuso.supervisor;
 
-import br.inatel.stazhi.execoes.UsuarioJaExisteException.UsuarioJaExisteException;
 import br.inatel.stazhi.repositorio.alunoRepositorio.AlunoRepositorio;
-import br.inatel.stazhi.repositorio.supervisorRepositorio.SupervisorRepositorio;
 
 public class SupervisionarAluno {
-     public void executar(int alunoId, int supervisorId ) throws UsuarioJaExisteException {
-        SupervisorRepositorio repoSupervisor = new SupervisorRepositorio();
+     public void executar(int alunoId, int supervisorId ){
         AlunoRepositorio repoAluno = new AlunoRepositorio();
         
         repoAluno.atualizarSupervisorId(alunoId, supervisorId);
